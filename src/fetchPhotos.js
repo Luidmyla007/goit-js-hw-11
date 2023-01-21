c5f5200acb57ba2801415bfeefade08a5dc0a0ec
Liudmyla007 <ldatsenko3@gmail.com>
2023-01-21 23:12:51 +0200

    try pagination

--- src/fetchPhotos.js
+++ src/fetchPhotos.js
@@ -5,13 +5,16 @@
 import axios from 'axios';
 export { fetchPhotos };
 
+let page = 1;
+const perPage = 40;
+
 const BASE_URL = 'https://pixabay.com/api/';
 const MY_KEY = '32917546-69d1ddfd267a57e0819ed8262&';
 const PHOTO_PARAM = 'image_type=photo&orientation=horizontal&safesearch=true';
 
-async function fetchPhotos(name, page, perPage) {
+async function fetchPhotos(query, page = 1, perPage = 40) {
   const response = await axios.get(
-    `${BASE_URL}?key=${MY_KEY}&q=${name}&${PHOTO_PARAM}&page=${page}&per_page=${perPage}`)
+    `${BASE_URL}?key=${MY_KEY}&q=${query}&${PHOTO_PARAM}&page=${page}&per_page=${perPage}`)
   return response
 }
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import Notiflix from 'notiflix';
 import { fetchPhotos } from './fetchPhotos.js';
 import { renderGallery } from './renderGallery.js';
 
-
-const a = 276;
+const a = 210;
 console.log(a);
 
 const searchForm = document.querySelector(".search-form");
@@ -12,53 +11,53 @@ loadBtn.hidden = true;
 let page = 1;
 const perPage = 40;
 
-// loadBtn.addEventListener('click', onLoad);
-
-
-// fetchPhotos()
-//   .then(({ data }) => {
-//     console.log(data);
-//     renderGallery( data.hits )
-//     load.hidden = false
-//   })
-// .catch(error => console.log(error))
-
 
 
 searchForm.addEventListener('submit', onSubmit);
 function onSubmit(event) {
   event.preventDefault();
   const { searchQuery } = event.currentTarget.elements;
-  const name = searchQuery.value;
-     if (name === '') {
+  const query = event.currentTarget.searchQuery.value;
+     if (query === '') {
     return (myGallery.innerHTML = '')
   }
   
-  fetchPhotos(name, page, perPage)
+  fetchPhotos(query, page, perPage)
     .then(({ data }) => {
-      if (data.totalHits === 0) {
+         if (data.totalHits === 0) {
         emptyRequest()
       }
       else {
+        console.log(data);
         renderGallery(data.hits);
         if (data.totalHits > perPage) {
           loadBtn.hidden = false;
         }
       }
-      })
-       .catch(error => console.log(error))
+    })
+    .catch(emptyRequest)
+    
     }
 
+loadBtn.addEventListener('click', onLoad);
+function onLoad() {
+  page += 1;
+  fetchPhotos(query, page, perPage).then(({ data }) => {
+    renderGallery(data.hits);
+    const totalPages = Math.ceil(data.totalHits / perPage)
+
+      if (page > totalPages) {
+        loadBtn.hidden = true;
+        return Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results.");
+        
+      }
+   
+    })
+  .catch(emptyRequest)
+}
 
 
-// async function getUser() {
-//   try {
-//     const response = await axios.get('/user?ID=12345');
-//     console.log(response);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
 
 function emptyRequest() {
   Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
